0891ae05ec8892bc0f971bb695f1d7b7415eb068
Jake Geiser <jgeiser@uncc.edu>
2021-02-26 16:24:19 -0500

    given question 2

--- EmployeeSQL/query/GivenQuestions.sql
+++ EmployeeSQL/query/GivenQuestions.sql
@@ -1,6 +1,12 @@
 
 --<1>List Employee Num, Last Name, First Name, Sex, and Salary
-SELECT e.emp_no, e.last_name, e.first_name, s.salary
+SELECT e.emp_no, e.last_name, e.first_name ,e.sex, s.salary
 	FROM employees AS e INNER JOIN salaries as s
 	ON e.emp_no = s.emp_no;
 --</1>
+
+--<2>List First Name, Last Name, and hire date for 1986 employees
+SELECT first_name, last_name, hire_date
+	FROM employees
+	WHERE YEAR(hire_date) = 1986;
+--</2>
